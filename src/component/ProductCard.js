571073588c1e80d4c46e8958be0a81c94b4b24fb
jong_min <whdals0525@gmail.com>
2024-03-27 17:06:58 +0900

    first-hnm-react-router-practice

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://lp2.hm.com/hmgoepprod?set=source[/e1/ab/e1ab4089432877f7f5b07fea41db2fcc008d9333.jpg],origin[dam],category[men_tshirtstanks_printed],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]' />
+      <div>Conscious choice</div>
+      <div>벨티드 트윌 코트</div>
+      <div>$99900</div>
+      <div>신제품</div>
+    </div>
+  );
+};
+
+export default ProductCard;
