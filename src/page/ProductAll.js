e5e2e6b9836baf39453758c424af840336922c09
jong_min <whdals0525@gmail.com>
2024-03-28 18:58:36 +0900

    four-hnm-react-router-practice

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,7 +5,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    let url = 'http://localhost:4000/products';
+    let url = `https://my-json-server.typicode.com/sonjongmin1/hnm-react-router-practice/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
