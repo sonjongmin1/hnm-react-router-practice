1f620028758bd1a925d7dfcc4a17c73428c827d9
jong_min <whdals0525@gmail.com>
2024-03-28 19:11:04 +0900

    five-hnm-react-router-practice

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -4,6 +4,9 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  // searchQuery 상태 추가
+  const [searchQuery, setSearchQuery] = useState('');
+
   const getProducts = async () => {
     let url = `https://my-json-server.typicode.com/sonjongmin1/hnm-react-router-practice/products?q=${searchQuery}`;
     let response = await fetch(url);
@@ -13,13 +16,16 @@ const ProductAll = () => {
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [searchQuery]); // searchQuery가 변경될 때마다 호출되도록 useEffect 업데이트
+
   return (
     <div>
       <Container>
         <Row>
           {productList.map((menu) => (
-            <Col lg={3}>
+            <Col lg={3} key={menu.id}>
+              {' '}
+              {/* 각 항목에 고유한 key 추가 */}
               <ProductCard item={menu} />
             </Col>
           ))}
