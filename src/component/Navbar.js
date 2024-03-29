f0cd5d572ef348d124ef2ddf988c0136f5fe3f5f
jong_min <whdals0525@gmail.com>
2024-03-30 01:46:26 +0900

    ten-hnm-react-router-practice

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const menuList = [
@@ -16,34 +16,58 @@ const Navbar = () => {
     '지속가능성',
   ];
 
-  const navigate = useNavigate();
+  let [width, setWidth] = useState(0);
+  let navigate = useNavigate();
   const goToLogin = () => {
     navigate('/login');
   };
+
+  const search = (event) => {
+    if (event.key === 'Enter') {
+      console.log('we click this key,', event.key);
+      //입력한 검색어를 읽어와서
+      let keyword = event.target.value;
+      console.log('keyword', keyword);
+      //url을 바꿔준다
+      navigate(`/?q=${keyword}`);
+    }
+  };
+
   return (
     <div>
-      <div>
-        <div class='login-button' onClick={goToLogin}>
-          <FontAwesomeIcon icon={faUser} />
-          <div>로그인</div>
+      <div className='side-menu' style={{ width: width }}>
+        <button className='closebtn' onClick={() => setWidth(0)}>
+          &times;
+        </button>
+        <div className='side-menu-list' id='menu-list'>
+          {menuList.map((menu, index) => (
+            <button key={index}>{menu}</button>
+          ))}
         </div>
       </div>
-      <div className='nav-section'>
-        <img
-          width={100}
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png'
-          alt=''
-        />
-      </div>
-      <div className='menu-area'>
-        <ul className='menu-list'>
-          {menuList.map((menu) => (
-            <li>{menu}</li>
-          ))}
-        </ul>
-        <div className='search-box'>
-          <FontAwesomeIcon icon={faSearch} />
-          <input type='text' placeholder='제품검색' />
+      <div className='nav-header'>
+        <div className='burger-menu hide'>
+          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+        </div>
+        <div className='nav-section'>
+          <Link to='/'>
+            <img
+              width={100}
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png'
+              alt=''
+            />
+          </Link>
+        </div>
+        <div className='menu-area'>
+          <ul className='menu-list'>
+            {menuList.map((menu, index) => (
+              <li key={index}>{menu}</li>
+            ))}
+          </ul>
+          <div className='search-box'>
+            <FontAwesomeIcon icon={faSearch} />
+            <input type='text' onKeyPress={(event) => search(event)} />
+          </div>
         </div>
       </div>
     </div>
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import ProductCard from '../component/ProductCard';
 import { Container, Row, Col } from 'react-bootstrap';
+import ProductCard from '../component/ProductCard';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   // searchQuery 상태 추가
-  const [searchQuery, setSearchQuery] = useState('');
-
+  const [query, setQuery] = useSearchParams();
   const getProducts = async () => {
+    let searchQuery = query.get('q') || '';
+    console.log('쿼리값은?', searchQuery);
     let url = `https://my-json-server.typicode.com/sonjongmin1/hnm-react-router-practice/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
@@ -16,7 +18,7 @@ const ProductAll = () => {
 
   useEffect(() => {
     getProducts();
-  }, [searchQuery]); // searchQuery가 변경될 때마다 호출되도록 useEffect 업데이트
+  }, [query]); // searchQuery가 변경될 때마다 호출되도록 useEffect 업데이트
 
   return (
     <div>
